2f0c34c5a1833fc987b6d8296d2cc967dc9d4d94
alex <122829843+itsal3xis@users.noreply.github.com>
2024-01-21 18:26:37 -0500

    Initial commit

---
+++ PATCHNOTES.md
@@ -0,0 +1,3 @@
+### Patch Notes 
+#### Version 0.1
+
---
+++ game.py
@@ -0,0 +1,38 @@
+from random import randint
+
+def cha():
+    print("\n\n\nLets start by writing your name !\n")
+    name = str(input(">  "))
+
+    invalid_car = ["!", "@", "#", "$", "%", "?", '&', '*', '(' , ')', '_', '=', '+', ':', ';', '"']
+
+    for letter in name:
+        if letter in invalid_car:
+            if_invalid = False
+            
+            while if_invalid == False:
+                print("Invalid name, choose another one ")
+                name = input(">  ")
+                if_invalid = True
+
+    name = name[0].upper() + name[1:].lower()
+
+    print("Choose your apptitude:\n1: Luckier\n2: Stronger\n3: Heavier\n")
+    choice = int(input(">  "))
+
+    while choice < 1 or choice > 3:
+        print("Invalid choice, choose again")
+        choice = int(input(">  "))
+
+    if choice == 1:
+        choice = "Luckier"
+    elif choice == 2:
+        choice = "Stronger"
+    else:
+        choice = "Heavier"
+
+    return name, choice
+
+
+name, boost = cha()
+
