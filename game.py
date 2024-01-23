271206eb479227d5262e07660a72f4e540ace843
alex <122829843+itsal3xis@users.noreply.github.com>
2024-01-23 10:09:29 -0500

    Tutorial / MacOS and Windows

--- game.py
+++ game.py
@@ -1,8 +1,9 @@
 from random import randint
 import time
+import os
 
 def cha():
-    print("\n\n\nLets start by writing your name !\n")
+    print("\n\n\n\n\nLets start by writing your name !\n")
     name = str(input(">  "))
 
     invalid_car = ["!", "@", "#", "$", "%", "?", '&', '*', '(' , ')', '_', '=', '+', ':', ';', '"']
@@ -18,7 +19,7 @@ def cha():
 
     name = name[0].upper() + name[1:].lower()
 
-    print("Choose your apptitude:\n1: Luckier\n2: Stronger\n3: Heavier\n")
+    print("\nChoose your apptitude:\n1: Luckier\n2: Stronger\n3: Heavier\n")
     choice = int(input(">  "))
 
     while choice < 1 or choice > 3:
@@ -35,18 +36,37 @@ def cha():
     return name, choice
 
 
+def tutorial():
+    os.system('cls')
+    os.system('clear')
+    time.sleep(2)
+    print("Stranger - Hey...")
+    time.sleep(2)
+    print(f"Stranger - Wake up {name} !")
+    time.sleep(2)
+    print(f"{name} - Ahhhh ..")
+    time.sleep(3)
+    print(f"{name} - What is that ?")
+    time.sleep(2)
+
+
+
 
 name, boost = cha()
+tuto = int(input("\n\nIs it your first time playing ?\n1: Yes\n2: No, skip the tutorial\n>  "))
 
-time.sleep(2)
-print("Stranger - Hey...")
-time.sleep(2)
-print(f"Stranger - Wake up {name} !")
-time.sleep(2)
-print(f"{name} - Ahhhh ..")
-time.sleep(3)
-print(f"{name} - What is that ?")
-time.sleep(2)
+while tuto < 1 or tuto > 2:
+    tuto = int("\nSelect a valid option >  ")
+
+if tuto == 1:
+    tutorial()
+else:
+    os.system('cls')
+    os.system('clear')
+
+
+
+
 
 
 
