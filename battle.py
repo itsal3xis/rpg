f6202692f8b5fb13f03016da8c1296888248eb11
alex <122829843+itsal3xis@users.noreply.github.com>
2024-02-28 08:08:31 -0500

    Create battle.py

---
+++ battle.py
@@ -0,0 +1,10 @@
+import random
+
+def intro_battle(name, boost):
+    #Tutorial for the battle
+    bossHp = 100
+    bossName = 'Boss'
+    bossAtk = 15
+    bossAbilities = ['Poison', 'Bad Omen']
+    
+
