38a4f3d4bc5da717695d8464ef0eefc85feb302e
alex <122829843+itsal3xis@users.noreply.github.com>
2024-03-04 15:27:23 -0500

    Update battle.py

--- battle.py
+++ battle.py
@@ -2,6 +2,11 @@ import random
 
 def intro_battle():
     #Tutorial for the battle
+    def playerTurn():
+        pass
+
+    def bossTurn():
+        pass
     class Boss:
         name = 'Boss'
         atk = 5
@@ -12,6 +17,21 @@ def intro_battle():
     bossintro = Boss
 
     print(f"Player vs {bossintro.name}")
+
+    choiceOfTurn = random.randint(1,2)
+    if choiceOfTurn == 1:
+        firstPick = 'player'
+    else:
+        firstPick = 'boss'
+
+    if firstPick == 'player':
+        while True:
+            playerTurn()
+            bossTurn()
+    else:
+        while True:
+            bossTurn()
+            playerTurn()
 
 
 
